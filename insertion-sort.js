c5e0733227229983104eb514a7fad568e5fe5a01
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2021-04-08 16:25:45 +0300

    First commit

---
+++ insertion-sort.js
@@ -0,0 +1,21 @@
+// Insertion Sort Pseudocode
+
+// Start by picking the second element in the array
+// Compare the second element with the one before it and swap if necessary
+// Continue to the next element and if it is in the incorrect order, iterate
+// through the sorted portion (i.e. the left side) to place the element in
+// the correct position.
+// Repeat until the array is sorted
+
+function insertionSort(arr) {
+	for (let i = 1; i < arr.length; i++) {
+		let currentVal = arr[i];
+		for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+			arr[j + 1] = arr[j];
+			arr[j] = currentVal;
+		}
+	}
+	return arr;
+}
+
+console.log(insertionSort([ 2, 1, 9, 76, 5 ]));
